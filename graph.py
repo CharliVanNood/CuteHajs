06180505134ddd6b7808c5683b6f55fcdd907e8e
Charli <charli.vannood@gmail.com>
2025-08-27 23:49:18 +0200

    finished graphing

--- graph.py
+++ graph.py
@@ -1,20 +1,29 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
 
 def graph(country):
     with open(f"histories/{country}", "r") as f:
         data = f.read().split("\n")
     dates = []
     prices = []
+    
     for line in data:
         dataSplit = line.split("x")
         if len(dataSplit) < 2: continue
         dates.append(dataSplit[1])
         prices.append(dataSplit[0])
 
-    plt.figure(figsize=(6,4))
+    dates = [datetime.fromtimestamp(int(float(ts))) for ts in dates]
+
+    plt.figure(figsize=(6,3))
     plt.plot(dates, prices, marker='o', linestyle='-', linewidth=2)
-    plt.title("Example Graph")
-    plt.xlabel("X Axis")
-    plt.ylabel("Y Axis")
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%Y/%m/%d"))
+    plt.gcf().autofmt_xdate(rotation=20)
+
+    plt.title(country)
+    plt.xlabel("")
+    plt.ylabel("Price")
 
     plt.savefig(f"graphs/graph{country}.png")
